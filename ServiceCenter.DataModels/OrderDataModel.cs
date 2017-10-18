26f1e09640c0207f7d3dc9379de17c1782cfd9bd
DSilkovich <dmitrysilkovich@gmail.com>
2017-10-18 23:16:42 +0300

    EF installed CodeFirst model added

---
+++ ServiceCenter.DataModels/OrderDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.DataModels
+{
+    public class OrderDataModel
+    {
+        public string Id { get; set; }
+        public string Device { get; set; }
+        public string Manufacturer { get; set; }
+        public string DeviceModel { get; set; }
+        public string SerialNumber { get; set; }
+        public bool Urgently { get; set; }
+
+    }
+}
---
+++ ServiceCenter.DataModels/ServiceCenterContext.cs
@@ -0,0 +1,31 @@
+﻿namespace ServiceCenter.DataModels
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ServiceCenterContext : DbContext
+    {
+        // Контекст настроен для использования строки подключения "ServiceCenterContext" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "ServiceCenter.DataModels.ServiceCenterContext" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "ServiceCenterContext" 
+        // в файле конфигурации приложения.
+        public ServiceCenterContext()
+            : base("name=ServiceCenterContext")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<OrderDataModel> Orders { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
