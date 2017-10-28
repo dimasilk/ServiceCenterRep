218d9d2c4330f030c8f3691a4843fe0b141d18f8
D.Silkovich <dmitrysilkovich@gmail.com>
2017-10-28 19:09:53 +0300

    Added client project with bootstrapper

---
+++ ServiceCenter.UI.Shell/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ServiceCenter.UI.Shell
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            this.ShutdownMode = ShutdownMode.OnMainWindowClose;
+            new ServiceCenterBootstrapper().Run();
+        }
+    }
+}
---
+++ ServiceCenter.UI.Shell/ServiceCenterBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+
+namespace ServiceCenter.UI.Shell
+{
+    public class ServiceCenterBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            Application.Current.MainWindow = this.Container.Resolve<Shell>();
+            return Application.Current.MainWindow;
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+        
+    }
+}
