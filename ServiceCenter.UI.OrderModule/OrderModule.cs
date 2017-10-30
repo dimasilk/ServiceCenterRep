ba09e9c7ead937afd83afa473df4c81fce6a6996
D.Silkovich <dmitrysilkovich@gmail.com>
2017-10-30 22:17:08 +0300

    Prism module added

--- ServiceCenter.UI.OrderModule/OrderModule.cs
+++ ServiceCenter.UI.OrderModule/OrderModule.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Unity;
 
 namespace ServiceCenter.UI.OrderModule
 {
+    [Module(ModuleName = "OrderModule")]
     public class OrderModule : IModule
     {
+        private readonly IUnityContainer _container;
+        public OrderModule(IUnityContainer container)
+        {
+            _container = container;
+        }
         public void Initialize()
         {
             throw new NotImplementedException();
--- ServiceCenter.UI.OrderModule/OrderServiceClient.cs
+++ ServiceCenter.UI.OrderModule/OrderServiceClient.cs
@@ -1,11 +1,40 @@
-﻿using System.ServiceModel;
+﻿using System;
 using ServiceCenter.BL.Common;
+using ServiceCenter.BL.Common.DTO;
 using ServiceCenter.UI.Infrastructure;
 
 namespace ServiceCenter.UI.OrderModule
 {
-    public class OrderServiceClient : WcfClientBase<IWcfOrderService>
+    public class OrderServiceClient : WcfClientBase<IWcfOrderService>, IWcfOrderService
     {
-        
+        public OrderDTO[] GetAllOrders()
+        {
+            return Channel.GetAllOrders();
+        }
+
+        public void UpdateOrder(OrderDTO orderModel)
+        {
+            Channel.UpdateOrder(orderModel);
+        }
+
+        public void AddOrder(OrderDTO orderModel)
+        {
+            Channel.AddOrder(orderModel);
+        }
+
+        public void DeleteOrder(Guid orderId)
+        {
+            Channel.DeleteOrder(orderId);
+        }
+
+        public OrderDTO GetOrderById(Guid orderId)
+        {
+            return Channel.GetOrderById(orderId);
+        }
+
+        public OrderDTO[] GetOrdersByUserId(Guid userId)
+        {
+            return Channel.GetOrdersByUserId(userId);
+        }
     }
 }
