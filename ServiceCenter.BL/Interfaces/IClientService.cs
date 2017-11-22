7d6ef528d940dc3b51a4ad87d7a288adedcfc176
D.Silkovich <dmitrysilkovich@gmail.com>
2017-11-22 22:14:41 +0300

    Interfaces for clients and companies

--- ServiceCenter.BL/Interfaces/IClientService.cs
+++ ServiceCenter.BL/Interfaces/IClientService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using ServiceCenter.BL.Common.DTO;
 
 namespace ServiceCenter.BL.Interfaces
 {
     public interface IClientService
     {
+        ClientDTO[] GetAllClients();
+        void DeleteClient(ClientDTO client);
+        void UpdateClient(ClientDTO client);
+        Guid AddClient(ClientDTO client);
+        ClientDTO GetClientById(Guid clientId);
+        //ClientDTO[] GetClientsByUserId(Guid userId);
     }
 }
--- ServiceCenter.BL/Interfaces/ICompanyService.cs
+++ ServiceCenter.BL/Interfaces/ICompanyService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServiceCenter.BL.Common.DTO;
 
 namespace ServiceCenter.BL.Interfaces
 {
     public interface ICompanyService
     {
+        CompanyDTO[] GetAllCompanies();
+        void DeleteCompany(CompanyDTO company);
+        void UpdateCompany(CompanyDTO company);
+        Guid AddCompany(CompanyDTO company);
+        CompanyDTO GetCompanyById(Guid companyId);
+        //CompanyDTO[] GetCompaniseByUserId(Guid userId);
     }
 }
