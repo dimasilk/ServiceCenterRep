eb543b429d649e55d9950dcb7ea06d8c5177ce48
D.Silkovich <dmitrysilkovich@gmail.com>
2017-11-23 23:40:20 +0300

    Added Customer module on UI

---
+++ ServiceCenter.UI.CustomerModule/CustomerModule.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using ServiceCenter.BL.Common;
+using ServiceCenter.UI.Infrastructure.Constants;
+using ServiceCenter.UI.OrderModule.View;
+
+namespace ServiceCenter.UI.CustomerModule
+{
+    [Module(ModuleName = nameof(CustomerModule))]
+    public class CustomerModule : IModule
+    {
+        private readonly IUnityContainer _container;
+        public CustomerModule(IUnityContainer container)
+        {
+            _container = container;
+        }
+        public void Initialize()
+        {
+            _container.RegisterType<IWcfCustomerService, CustomerServiceClient>(new ContainerControlledLifetimeManager());
+            var rm = _container.Resolve<IRegionManager>();
+            //rm.RegisterViewWithRegion(RegionNames.MainRegion, typeof(OrderView));
+            rm.RegisterViewWithRegion(RegionNames.MenuRegion, typeof(OrderToolbarView));
+        }
+    }
+}
---
+++ ServiceCenter.UI.CustomerModule/CustomerServiceClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceCenter.BL.Common;
+using ServiceCenter.BL.Common.DTO;
+using ServiceCenter.UI.Infrastructure;
+
+namespace ServiceCenter.UI.CustomerModule
+{
+    public class CustomerServiceClient : IWcfCustomerService
+    {
+        public Task<CustomerDTO[]> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCustomer(Guid clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomer(CustomerDTO client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid AddCustomer(CustomerDTO client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerDTO GetCustomerById(Guid clientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
