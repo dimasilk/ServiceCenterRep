4fea18fe5425f697a2d0536c7bd270eab9224ebb
D.Silkovich <dmitrysilkovich@gmail.com>
2017-12-07 23:23:26 +0300

    Validation

---
+++ ServiceCenter.UI.Infrastructure/Validation/RequiredFieldValidationRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ServiceCenter.UI.Infrastructure.Validation
+{
+    public class RequiredFieldValidationRule : ValidationRule
+    {
+        public string Field { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrEmpty((string) value)) return new ValidationResult(false, "Field " + Field + " is required");
+            return new ValidationResult(true, null);
+        }
+    }
+}
